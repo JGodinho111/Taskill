b0bba6220a673d98136ab9736b3fd51d3c7c48c6
JGodinho111 <jgodinho111@gmail.com>
2021-12-14 20:52:58 +0000

    Getting Ready for Firebase Auth

--- app/src/main/java/com/example/taskill/RegisterActivity.java
+++ app/src/main/java/com/example/taskill/RegisterActivity.java
@@ -2,38 +2,36 @@ package com.example.taskill;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class LogInActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
-    @SuppressLint({"ResourceAsColor", "Range"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
-        getWindow().getDecorView().setBackgroundColor(Color.parseColor("#20232A"));
+        setContentView(R.layout.activity_register);
 
-        Button b = (Button)findViewById(R.id.buttonLogIn);
+        Button b = (Button)findViewById(R.id.buttonRegister);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LogInActivity.this,MainActivityBot.class));
+                startActivity(new Intent(RegisterActivity.this,MainActivityBot.class));
                 //startActivity(new Intent(LogInActivity.this,MainActivity.class));
             }
         });
 
-        TextView goToRegister = (TextView) findViewById(R.id.textViewGoToRegister);
+        TextView goToRegister = (TextView) findViewById(R.id.textViewGoToLogIn);
         goToRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LogInActivity.this,RegisterActivity.class));
+                startActivity(new Intent(RegisterActivity.this,LogInActivity.class));
             }
         });
     }
+
+
 }
